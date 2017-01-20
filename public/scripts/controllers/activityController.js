138ea0746dc5155510c4dd45d41cb5a46a2ad5fb
dkingdon <daniel.kingdon@gmail.com>
2017-01-20 13:37:10 -0800

    added initial shape logic. randomly selects shaps (activated by button) Displays shape on screen and directional text

--- public/scripts/controllers/activityController.js
+++ public/scripts/controllers/activityController.js
@@ -8,5 +8,18 @@ console.log('activityController.js is connected') //TODO: Revome before producti
     function activityController ($http) {
       var vm = this;
 
+      /* - - - temp - - - */
+      var shapes = ['square', 'circle', 'triangle', 'rectangle'];
+
+      var randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+
+      vm.displayShape = function() {
+        randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+        $('#pk-directions h1').text('What shape is this?');
+        $('#pk-board h2').text(randomShape);
+      }
+
+
+
 
     } // End of controller TODO: remove before production
